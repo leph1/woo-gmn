7bba0f42528a5c2c3dfe6668fd04fb2cc9128705
leph1 <phong.le1@gmail.com>
2021-01-25 17:14:57 +0000

    Added check for non integer input from user

--- woo.py
+++ woo.py
@@ -1,9 +1,20 @@
 import random
 
-low_num = int(input("Set the game parameters, enter the lowest number: "))
-high_num = int(input("Now enter the highest number: "))
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))       
+    except ValueError as e:
+       print(str(e)+" is not an integer! Please try again.")
+       continue
+    else:
+       return userInput 
+       break
+
+low_num = inputNumber("Set the game parameters, enter the lowest number: ")
+high_num = inputNumber("Now enter the highest number: ")
 my_number = random.randint(low_num,high_num)
-your_number = int(input("Guess my number (hint - it's an integer between "+str(low_num)+" and "+str(high_num)+"): "))
+your_number = inputNumber("Guess my number (hint - it's an integer between "+str(low_num)+" and "+str(high_num)+"): ")
 guess_count = 0
 
 while your_number != my_number:
@@ -16,4 +27,4 @@ while your_number != my_number:
 else:
     guess_count +=1
     print("That's amazing! I really was thinking of the number "+str(my_number))
-print("...And it only took you a measly "+str(guess_count)+" goes!")
+print("...And it only took you a measly "+str(guess_count)+" guesses!")
